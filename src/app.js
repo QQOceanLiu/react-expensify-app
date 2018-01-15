46041b1f280ad9edad6c45f5b70b53a29e98d6a9
Haiyang Liu <haiyangliu@lyft.com>
2018-01-15 10:47:29 -0800

    remove demo code

--- src/app.js
+++ src/app.js
@@ -15,11 +15,6 @@ store.subscribe(() => {
   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
   console.log(visibleExpenses);
 });
-
-store.dispatch(addExpense({description: 'water', amount: 400}));
-store.dispatch(addExpense({description: 'gas', amount: 800, createdAt:1000}));
-store.dispatch(addExpense({description: 'rent', amount: 105000}));
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
